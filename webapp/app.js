d97eada7898d40e4174a71616eb9912fe28834e1
Marcello Luna <cellolunam@gmail.com>
2023-02-08 11:10:36 -0600

    moving to ejs. early commit for caytlin

--- webapp/app.js
+++ webapp/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+app.set('view engine', 'ejs');
 app.listen(3000);
 
 // app.get('/', (req, res) => {
@@ -8,13 +9,25 @@ app.listen(3000);
 // app.get('/Add-item', (req, res) => {
 //     res.send('<p>Add-item</p>');
 // });
-
+/* 
 app.get('/', (req, res) => {
-    res.sendFile('/views/index.html', {root:__dirname});
+    res.sendFile('/views/index.ejs', {root:__dirname});
 });
 app.get('/add-item', (req, res) => {
-    res.sendFile('/views/addItem.html', {root:__dirname});
+    res.sendFile('/views/addItem.ejs', {root:__dirname});
 });
 app.use((req, res) => {
-    res.sendFile('/views/error.html', {root:__dirname});
-});
+    res.sendFile('/views/error.ejs', {root:__dirname});
+}); 
+*/
+
+app.get('/', (req, res) => {
+    res.render('index', {title: "CRUD APP"});
+});  
+
+app.get('/add-item', (req, res) => {
+    res.render('addItem');
+});  
+app.use((req, res) => {
+    res.render('error');
+});  
