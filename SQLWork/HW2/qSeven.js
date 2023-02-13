e831cc1700d60fb59346d4494cc234451c8d1d8a
Marcello Luna <cellolunam@gmail.com>
2023-02-13 10:14:05 -0600

    fixed q7

--- SQLWork/HW2/qSeven.js
+++ SQLWork/HW2/qSeven.js
@@ -10,7 +10,7 @@ var con = mysql.createConnection({
 con.connect(function(err){
     if (err) throw err;
     var salarySelect = ("SELECT * FROM salary_grade");
-    var salaryUpdate = ("UPDATE salary_grade SET salary = salary + 2000 WHERE employeeId = (SELECT s.employeeId FROM salary_grade s JOIN employees e ON s.employeeId = e.id WHERE e.yearsWithCompany > 3)");
+    var salaryUpdate = ("UPDATE salary_grade SET salary = salary + 2000 WHERE employeeId IN (SELECT s.employeeId FROM salary_grade s JOIN employees e ON s.employeeId = e.id WHERE e.yearsWithCompany > 3)");
 
     con.query(salarySelect, function(err, res) {
         if (err) throw err;
