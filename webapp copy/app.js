f587e2bbeda4c9b996a398ac0072d09e6baa59eb
Marcello Luna <cellolunam@gmail.com>
2023-02-13 12:02:07 -0600

    creating partial views in the webapp

---
+++ webapp copy/app.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const app = express();
+app.set("view engine", "ejs");
+app.listen(3000);
+
+// app.get('/', (req, res) => {
+//     res.send('<p>Home</p>');
+// });
+// app.get('/Add-item', (req, res) => {
+//     res.send('<p>Add-item</p>');
+// });
+/* 
+app.get('/', (req, res) => {
+    res.sendFile('/views/index.ejs', {root:__dirname});
+});
+app.get('/add-item', (req, res) => {
+    res.sendFile('/views/addItem.ejs', {root:__dirname});
+});
+app.use((req, res) => {
+    res.sendFile('/views/error.ejs', {root:__dirname});
+}); 
+*/
+const item = [
+  { name: "Marcello", lName: "Luna" },
+  { name: "Evan", lName: "Sambrugaro" },
+  { name: "Calvin", lName: "Newlander" },
+  { name: "Caytlin", lName: "Lindeland" },
+];
+
+app.get("/", (req, res) => {
+  res.render("index", { item });
+});
+
+app.get("/add-item", (req, res) => {
+  res.render("addItem");
+});
+app.use((req, res) => {
+  res.render("error");
+});
--- webapp/app.js
+++ webapp/app.js
@@ -1,39 +1,22 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 app.listen(3000);
 
-// app.get('/', (req, res) => {
-//     res.send('<p>Home</p>');
-// });
-// app.get('/Add-item', (req, res) => {
-//     res.send('<p>Add-item</p>');
-// });
-/* 
-app.get('/', (req, res) => {
-    res.sendFile('/views/index.ejs', {root:__dirname});
-});
-app.get('/add-item', (req, res) => {
-    res.sendFile('/views/addItem.ejs', {root:__dirname});
-});
-app.use((req, res) => {
-    res.sendFile('/views/error.ejs', {root:__dirname});
-}); 
-*/
 const item = [
-    {name: "Marcello", lName: "Luna"},
-    {name: "Evan", lName: "Sambrugaro"},
-    {name: "Calvin", lName: "Newlander"},
-    {name: "Caytlin", lName: "Lindeland"},
-]
+  { name: "IPhone 8", price: 800 },
+  { name: "Pixel 7", price: 600 },
+  { name: "Galxy S23", price: 1200 },
+  { name: "Galaxy Z-Flip", price: 1500 },
+];
 
-app.get('/', (req, res) => {
-    res.render('index', {item});
-});  
+app.get("/", (req, res) => {
+  res.render("index", { item });
+});
 
-app.get('/add-item', (req, res) => {
-    res.render('addItem');
-});  
+app.get("/add-item", (req, res) => {
+  res.render("addItem");
+});
 app.use((req, res) => {
-    res.render('error');
-});  
+  res.render("error");
+});
