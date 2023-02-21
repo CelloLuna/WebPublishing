2f7feb2ee6dc7d2b5f3d4b11ad2b9b758b4913dc
Marcello Luna <cellolunam@gmail.com>
2023-02-21 14:24:48 -0600

    class work

--- webapp/app.js
+++ webapp/app.js
@@ -3,6 +3,7 @@ const app = express();
 const mongoose = require("mongoose");
 const Item = require("./models/item.js");
 app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
 const mongodb = "mongodb://127.0.0.1:27017/Lab2";
 // const mongodb = "mongodb+srv://lunmars:lunmars1@cluster0.pnfpvdv.mongodb.net/cello?retryWrites=true&w=majority";
 mongoose.set("strictQuery", false);
@@ -14,34 +15,51 @@ mongoose
   })
   .catch((e) => console.log("ERROR: " + e));
 
-app.get("/create-item", (req, res) => {
-  const item = new Item({
-    name: "Galaxy S23",
-    price: 1200,
-  });
-
-  item
-    .save()
-    .then((result) => res.send(result))
-    .catch((e) => console.log("ERROR: " + e));
-});
-app.get("/get-item", (req, res) => {
-  Item.find()
-    .then((result) => res.send(result))
-    .catch((e) => console.log("ERROR: " + e));
-});
-app.get("/get-item-by-id", (req, res) => {
-  Item.findById("63ebc338807ea1c35b13b311")
-    .then((result) => res.send(result))
-    .catch((e) => console.log("ERROR: " + e));
-});
 app.get("/", (req, res) => {
-  res.render("index", { item });
+  Item.find()
+    .then((result) => {
+      res.render("index", { item: result });
+    })
+    .catch((e) => console.log("ERROR: " + e));
 });
-
 app.get("/add-item", (req, res) => {
   res.render("addItem");
 });
+
+app.post("/items", (req, res) => {
+  console.log(req.body);
+  const item = Item(req.body);
+  item
+    .save()
+    .then(() => {
+      res.redirect("/");
+    })
+    .catch((e) => console.log("ERROR: " + e));
+});
+
 app.use((req, res) => {
   res.render("error");
 });
+
+// app.get("/create-item", (req, res) => {
+//   const item = new Item({
+//     name: "Galaxy S23",
+//     price: 1200,
+//   });
+
+//   item
+//     .save()
+//     .then((result) => res.send(result))
+//     .catch((e) => console.log("ERROR: " + e));
+// });
+
+// app.get("/get-item", (req, res) => {
+//   Item.find()
+//     .then((result) => res.send(result))
+//     .catch((e) => console.log("ERROR: " + e));
+// });
+// app.get("/get-item-by-id", (req, res) => {
+//   Item.findById("63ebc338807ea1c35b13b311")
+//     .then((result) => res.send(result))
+//     .catch((e) => console.log("ERROR: " + e));
+// });
