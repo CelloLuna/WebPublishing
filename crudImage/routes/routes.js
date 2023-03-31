ac0476be186595b6011390b6748dfda59f0dc1cc
Marcello Luna <cellolunam@gmail.com>
2023-03-30 20:00:56 -0500

    image crud updates

--- crudImage/routes/routes.js
+++ crudImage/routes/routes.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const router = express.Router();
-
+title = '';
+router.get('/', (req, res) => {
+  title = 'Home Page';
+  res.render('index', { title });
+});
 router.get('/users', (req, res) => {
   res.send('All users');
 });
-router.get('/', (req, res) => {
-  res.send('Home Page');
+router.get('/add', (req, res) => {
+  title = 'Add Users';
+  res.render('addUsers', { title });
 });
 
 module.exports = router;
