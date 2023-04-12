331c26ae97fb06bcdd3a5e0d31721f7e335f8222
Marcello Luna <cellolunam@gmail.com>
2023-04-12 10:58:45 -0500

    registration validation errors

--- userLoginApp/routes/users.js
+++ userLoginApp/routes/users.js
@@ -16,7 +16,23 @@ router.post('/register', (req, res) => {
 
   //check required fields
   if (!name || !email || !password || !password2) {
-    errors.push({ mes: 'Please enter all fields' });
+    errors.push({ msg: 'Please enter all fields' });
+  }
+  if (password != password2) {
+    errors.push({ msg: 'Passwords do not match' });
+  }
+  if (password.length < 6) {
+    errors.push({ msg: 'Password must be at least 6 characters' });
+  }
+  if (errors.length > 0) {
+    res.render('register', {
+      errors,
+      name,
+      email,
+      password,
+      password2,
+      title: 'Register',
+    });
   } else {
     res.send('pass');
   }
